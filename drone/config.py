c8e6cbd55e495afabe1e7227934b46794f927fc9
showind <simon.howind@tuwien.ac.at>
2023-11-13 17:06:15 +0100

    Changed back time factor to 60 (1s = 1 min)

--- drone/config.py
+++ drone/config.py
@@ -1,5 +1,5 @@
 
-resolution = 60 # resolution of a timestep in seconds
-max_power = 200 # max power in W
-simulation_time_factor = 180.0
+resolution = 60  # resolution of a timestep in seconds
+max_power = 200  # max power in W
+simulation_time_factor = 60.0
 slot_count = 48*60  # has to be multiple of 24 hours
